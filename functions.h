5246ca19edcc6ed734ef404d17d652936f4212ef
dtarnacki <damian.tarnacki@ugent.be>
2015-05-02 00:02:46 +0200

    Function definitions moved to header file

---
+++ functions.h
@@ -0,0 +1,24 @@
+// Functions
+void initVTK();
+void renderVTK();
+void setLOD(ushort lod);
+void prepareMenu();
+void loadVessels();
+void loadFile(VesselFile type);
+void setupSegmentedImagePlanes();
+void addToDistance(int point[3]);
+bool isSkeleton(VesselFile type);
+bool isEmpty(double point[3]);
+
+// UI functions
+void setDistanceText(string text);
+void drawDistanceLine();
+void resetDistancePoints();
+double computeDistance(double a[3], double b[3]);
+void toggleCommandsMenu();
+void toggleLoading();
+void toggleSegmentedTransparent();
+void togglePlane(ImagePlane planeToShow);
+void refreshWindow();
+
+void KeypressCallbackFunction (vtkObject* caller, long unsigned int eventId, void* clientData, void* callData);
--- main.cpp
+++ main.cpp
@@ -17,44 +17,20 @@
 #include <vtkCommand.h>
 #include <vtkCallbackCommand.h>
 
-// 
+// image planes
 #include <vtkCellPicker.h>
 #include <vtkStructuredPointsReader.h>
 #include <vtkStructuredPoints.h>
 
-// Point Picker
+// point Picker
 #include <vtkPointPicker.h>
 #include <vtkRendererCollection.h>
 #include <vtkObjectFactory.h>
 
 #include "MyImage3D.h"
+#include "functions.h"
 
 using namespace std;
-
-// Functions
-void initVTK();
-void renderVTK();
-void setLOD(ushort lod);
-void prepareMenu();
-void loadVessels();
-void loadFile(VesselFile type);
-void setupSegmentedImagePlanes();
-void addToDistance(int point[3]);
-bool isSkeleton(VesselFile type);
-bool isEmpty(double point[3]);
-
-// UI functions
-void setDistanceText(string text);
-void drawDistanceLine();
-void resetDistancePoints();
-double computeDistance(double a[3], double b[3]);
-void toggleCommandsMenu();
-void toggleLoading();
-void toggleSegmentedTransparent();
-void togglePlane(ImagePlane planeToShow);
-void refreshWindow();
-
-void KeypressCallbackFunction (vtkObject* caller, long unsigned int eventId, void* clientData, void* callData);
 
 // VTK window
 vtkSmartPointer<vtkRenderer> renderer = NULL;
